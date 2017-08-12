d98428511fc0ac81e02709915e2ef6ef1facd907
max cheung <maxcheung@optusnet.com.au>
2017-08-12 13:35:53 +1000

    unit test

--- comms/src/main/java/com/maxcheung/comms/Application.java
+++ comms/src/main/java/com/maxcheung/comms/Application.java
@@ -2,31 +2,12 @@ package com.maxcheung.comms;
 
 import javax.annotation.PostConstruct;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.thymeleaf.context.Context;
-
-import com.maxcheung.comms.email.EmailHtmlSender;
-import com.maxcheung.comms.email.EmailSender;
-import com.maxcheung.comms.email.EmailStatus;
-
-
+		
 
 @SpringBootApplication
 public class Application {
-	
-	
-    @Autowired
-    private EmailHtmlSender emailHtmlSender;
-    
-    @Autowired
-    private EmailSender emailSender;
-    
-    
-    @Value("${mail.to}")
-    private String mailTo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -36,11 +17,6 @@ public class Application {
 	@PostConstruct
 	public void init() {
 		// init code goes here
-		Context context = new Context();
-		context.setVariable("title", "Lorem Ipsum");
-		context.setVariable("description", "Lorem Lorem Lorem");
-		EmailStatus emailStatus = emailHtmlSender.send(mailTo, "Spring boot app", "email/template-1", context);
-		emailSender.sendPlainText(mailTo, "Spring boot app", "this is not samp");
 	}
 
 }
---
+++ comms/src/test/java/com/maxcheung/comms/ApplicationTests.java
@@ -0,0 +1,39 @@
+package com.maxcheung.comms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.context.Context;
+
+import com.maxcheung.comms.email.EmailHtmlSender;
+import com.maxcheung.comms.email.EmailSender;
+import com.maxcheung.comms.email.EmailStatus;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+	
+    @Autowired
+    private EmailHtmlSender emailHtmlSender;
+    
+    @Autowired
+    private EmailSender emailSender;
+    
+    
+    @Value("${mail.to}")
+    private String mailTo;
+
+
+	@Test
+	public void contextLoads() {
+		Context context = new Context();
+		context.setVariable("title", "Lorem Ipsum");
+		context.setVariable("description", "Lorem Lorem Lorem");
+		EmailStatus emailStatus = emailHtmlSender.send(mailTo, "Spring boot app", "email/template-1", context);
+		emailSender.sendPlainText(mailTo, "Spring boot app", "this is not samp");
+	}
+
+}
